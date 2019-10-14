e45d3d7e8ec96ed401fe872801aed2a5236dfffb
unknown <alejandrona5b@gmail.com>
2019-10-14 09:18:03 +0200

    9, 10

---
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] antonio = {"pepito", "pepino", "antonio", "carlos", "raul", "jopegras", "noelia", "mateo", "diccionario", "godofredo"};
+            int diferencia = 0;
+            String auxiliar = "";
+            for(int i = 0; i < antonio.Length; i++){
+                for(int j = 0; j < antonio.Length; j++){
+                    diferencia = string.Compare(antonio[i], antonio[j]);
+                    if(diferencia == -1){
+                        auxiliar = antonio[i];
+                        antonio[i] = antonio[j];
+                        antonio[j] = auxiliar;
+                    }
+                }
+            }
+
+            for(int i = 0; i < antonio.Length; i++)
+            {
+                Console.WriteLine(antonio[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String a = "";
+            String b = "";
+
+            int x = 0;
+            int y = 0;
+            int division = 0;
+
+            try
+            {
+                Console.Write("Introduce un numero: ");
+                x = Convert.ToInt32(a = Console.ReadLine());
+
+                Console.WriteLine("\nIntroduce otro numero: ");
+                y = Convert.ToInt32(b = Console.ReadLine());
+
+                division = x / y;
+                Console.WriteLine("El resultado de la divison --> ");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
