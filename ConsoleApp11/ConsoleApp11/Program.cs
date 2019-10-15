c9bb19dbf779cf113fc87affc04e229c478ab025
unknown <alejandrona5b@gmail.com>
2019-10-15 12:08:24 +0200

    Ultimo ejercicio

---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayPablo = new ArrayList();
+            Random r = new Random();
+            
+            int numero = 0;
+            for(int i = 0; i < 50; i++)
+            {
+                numero = r.Next(0, 50);
+                arrayPablo.Add(numero);
+            }
+
+            foreach (int i in arrayPablo)
+            {
+                Console.WriteLine(i);
+            }
+
+            arrayPablo.Sort();
+
+            foreach(int i in arrayPablo)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
