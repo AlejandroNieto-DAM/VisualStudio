776a9a953b9a4598d1a038445da3cf1d01238f3d
NietOff <alejandrona5b@gmail.com>
2019-11-06 00:36:46 +0100

    Algoritmo de reescritura de palabra falla

---
+++ WindowsFormsApp7/WindowsFormsApp7/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace WindowsFormsApp7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(104, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(52, 31);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Letra:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 264);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Letras Usadas:";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(35, 313);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(263, 222);
+            this.label3.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(34, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 23);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Palabra:";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(129, 166);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(306, 32);
+            this.label5.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(163, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Comprobar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Verdana", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(725, 60);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(236, 64);
+            this.label6.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1063, 609);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ WindowsFormsApp7/WindowsFormsApp7/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+
+        private String palabraSecreta = "";
+        private int intentos = 5;
+        ArrayList letrasUsadas = new ArrayList();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            palabraSecreta = "Hola";
+            for (int i = 0; i < palabraSecreta.Length - 1; i++)
+            {
+                label5.Text += "_";
+            }
+            label5.Text += "_";
+        }
+
+        public void comprobarLetra(Char letra)
+        {
+
+            Boolean encontrado = false;
+            Boolean encontradoRepe = false;
+
+            foreach (Char l in letrasUsadas)
+            {
+                if (l.Equals(letra))
+                {
+                    encontradoRepe = true;
+                }
+            }
+
+            letrasUsadas.Add(letra);
+
+            if(encontradoRepe == false)
+            {
+                foreach (Char l in palabraSecreta)
+                {
+                    if (l.Equals(letra))
+                    {
+                        encontrado = true;
+                        label6.Text = "La letra esta en la palabra!";
+                    }
+                }
+
+                if (encontrado == true)
+                {
+                    this.tratarLetra(letra);
+                }
+                else
+                {
+                    
+                    label3.Text += letra + " ";
+                    intentos--;
+                    if (intentos == 0)
+                    {
+                        label6.Text = "Game over!";
+                        button1.Enabled = false;
+                    }
+                }
+            }
+            
+            
+        }
+
+        public void tratarLetra(Char letra)
+        {
+            String palabraActualizada = label5.Text;
+            String palabraAct = "";
+         
+            Boolean encontrada = false;
+
+            foreach (Char l in palabraActualizada)
+            {
+                if (l.Equals('_'))
+                {
+                    
+                    foreach (Char l2 in palabraSecreta)
+                    {
+                        if (l2.Equals(letra))
+                        {
+                            palabraAct += l2;
+                        }
+                        else
+                        {
+                            palabraAct += "_";
+                        }
+                    }
+                }
+                else
+                {
+                    palabraAct += l;
+                }
+            }
+
+            label5.Text = "";
+            label5.Text = palabraAct;
+            
+
+          //  label5.Text = this.hacerPalabra(palabraActualizada);
+
+            if (palabraActualizada.Equals(palabraSecreta))
+            {
+                    
+                label6.Text = "Wnner!!";
+                button1.Enabled = false;
+
+            }
+            
+    
+        }
+
+        public String hacerPalabra(String palabra)
+        {
+            String palabra2 = "";
+            foreach (Char l in palabra)
+            {
+
+                palabra2 += l + "_";
+
+
+            }
+            
+            return palabra;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Char letra = Convert.ToChar(textBox1.Text);
+
+            this.comprobarLetra(letra);
+
+        }
+    }
+}
